06b148793f9de5383d3a9df128b38547f1c47f88
Fabian Szkudlarski <146945404+lyneq2001@users.noreply.github.com>
2025-06-27 04:45:23 +0200

    Add reservation feature and fix date formatting

--- library-management/src/main/java/com/library/service/ReservationService.java
+++ library-management/src/main/java/com/library/service/ReservationService.java
@@ -2,6 +2,7 @@ package com.library.service;
 
 import com.library.entity.Reservation;
 import com.library.entity.User;
+import com.library.entity.Book;
 import com.library.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,5 +24,13 @@ public class ReservationService {
     @Transactional(readOnly = true)
     public long countByUser(User user) {
         return reservationRepository.findByUser(user).size();
+    }
+
+    public Reservation reserveBook(User user, Book book) {
+        if (reservationRepository.existsByUserAndBook(user, book)) {
+            throw new RuntimeException("Reservation already exists");
+        }
+        Reservation reservation = new Reservation(user, book);
+        return reservationRepository.save(reservation);
     }
 }
